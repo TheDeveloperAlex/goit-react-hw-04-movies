a32b64eefb3f043f80969338b505e3aa6b2d2cfe
TheDeveloperAlex <83388713+TheDeveloperAlex@users.noreply.github.com>
2025-01-05 02:09:01 +0100

    refactor/init-new-project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.querySelector("#root")
+  </React.StrictMode>
 );
